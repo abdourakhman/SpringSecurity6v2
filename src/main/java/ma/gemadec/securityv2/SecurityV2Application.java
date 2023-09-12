df821e05f311931a63d23b1641532f6582c99331
abdourahmane NDIAYE <abdourakhman920@gmail.com>
2023-09-12 11:19:12 +0200

    first commit

---
+++ src/main/java/ma/gemadec/securityv2/SecurityV2Application.java
@@ -0,0 +1,13 @@
+package ma.gemadec.securityv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ma/gemadec/securityv2/SecurityV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.gemadec.securityv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
